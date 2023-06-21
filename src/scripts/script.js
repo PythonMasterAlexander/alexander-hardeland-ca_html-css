88caeb0b2317e1dce56bb11a5133522e0d78c03c
Alexander <alexanderhardeland@gmail.com>
2023-06-21 12:15:47 +0000

    Adds prettier as a dependency and two npm scripts to run and check project

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,8 @@
 import { locatePage } from "./component/components.js";
 import { navigationLinks } from "./component/components.js";
 
-navigationLinks.forEach(links => {
-  if(links.href.includes(`${locatePage}`)) {
-    links.classList.add('activeLink');
+navigationLinks.forEach((links) => {
+  if (links.href.includes(`${locatePage}`)) {
+    links.classList.add("activeLink");
   }
 });
